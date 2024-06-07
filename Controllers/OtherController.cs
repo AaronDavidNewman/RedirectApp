0c8c33a561fc2d1d0b1530194d7871287217da14
Aaron <aarondnewman@gmail.com>
2024-06-07 09:39:57 -0500

    initial commit for issue

---
+++ Controllers/OtherController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RedirectApp.Controllers
+{
+    public class OtherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
